2706d84b8a0192d26e9b11419fb8cebdc6387f10
Niels <niels.poensgen@gmail.com>
2016-03-10 19:10:53 +0100

    fixed some bugs and the wordlist now includes spaces

--- script.py
+++ script.py
@@ -22,27 +22,17 @@ for word in words:
 
 
 prefixes = words[prefixIt+1:suffixIt-1]
-suffixes = words[suffixIt+1:bothIt-1]
-both = words[bothIt+1:attachementIt-1]
+suffixes = words[suffixIt+1:attachementIt-1]
 attachements = words[attachementIt+1:]
 
 prefix = random.choice(prefixes)
 suffix = random.choice(suffixes)
-bothWord = random.choice(both)
 attachement = random.choice(attachements)
 
 
-random = randint(0,5)
+random = randint(0,1)
 
 if random == 0:
-	print prefix + " " + suffix
+	print prefix + suffix
 elif random == 1:
-	print prefix + " " + suffix + " " + attachement
-elif random == 2:
-	print bothWord + " " + suffix
-elif random == 3:
-	print bothWord + " " + suffix + " " + attachement
-elif random == 4:
-	print prefix + " " + bothWord
-elif random == 5:
-	print prefix + " " + bothWord + " " + attachement 
+	print prefix + suffix + " " + attachement
