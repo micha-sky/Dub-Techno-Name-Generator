f353e11fb3a0a0e0d65abf39c3394d7a2b34f50b
Niels <niels.poensgen@gmail.com>
2016-03-11 07:46:49 +0100

    refined word list

--- script.py
+++ script.py
@@ -3,33 +3,32 @@ from random import randint
 
 words = [line.rstrip('\n') for line in open('wordlist.txt')]
 
-prefixIt = 0
-suffixIt = 0
-attachementIt = 0
+prefixIndex = 0
+suffixIndex = 0
+attachementIndex = 0
 i = 0
 
 for word in words:
 	if word == "Prefixes:":
-		prefixIt = i
+		prefixIndex = i
 	elif word == "Suffixes:":
-		suffixIt = i
+		suffixIndex = i
 	elif word == "Attachement:":
-		attachementIt = i
+		attachementIndex = i
 	i = i + 1
 
 
-prefixes = words[prefixIt+1:suffixIt-1]
-suffixes = words[suffixIt+1:attachementIt-1]
-attachements = words[attachementIt+1:]
-
-prefix = random.choice(prefixes)
-suffix = random.choice(suffixes)
-attachement = random.choice(attachements)
+prefixes = words[prefixIndex+1:suffixIndex-1]
+suffixes = words[suffixIndex+1:attachementIndex-1]
+attachements = words[attachementIndex+1:]
 
 
-random = randint(0,1)
-
-if random == 0:
-	print prefix + suffix
-elif random == 1:
-	print prefix + suffix + " " + attachement
+for x in range(0,9):
+	prefix = random.choice(prefixes)
+	suffix = random.choice(suffixes)
+	attachement = random.choice(attachements)
+	rand = randint(0,3)
+	if rand <= 2:
+		print prefix + suffix
+	elif rand == 3:
+		print prefix + suffix + " " + attachement
